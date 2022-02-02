d0af09c02538dca2acd35d624c4c5d82fd36e432
Msksgm <msksgm3124@gmail.com>
2022-02-03 07:34:16 +0900

    add: get /articles/{slug} のテストを追加

--- mock/article.go
+++ mock/article.go
@@ -9,6 +9,7 @@ import (
 type ArticleService struct {
 	CreateArticleFn func(*model.Article) error
 	ArticleBySlugFn func(*model.Article) (*model.Article, error)
+	ArticlesFn      func() ([]*model.Article, error)
 }
 
 func (m *ArticleService) CreateArticle(_ context.Context, article *model.Article) error {
@@ -16,7 +17,7 @@ func (m *ArticleService) CreateArticle(_ context.Context, article *model.Article
 }
 
 func (m *ArticleService) Articles(_ context.Context, af model.ArticleFilter) ([]*model.Article, error) {
-	return nil, nil
+	return m.ArticlesFn()
 }
 
 func (m *ArticleService) ArticleBySlug(_ context.Context, slug string) (*model.Article, error) {
--- server/article_test.go
+++ server/article_test.go
@@ -70,6 +70,60 @@ func Test_createArticle(t *testing.T) {
 	}
 }
 
+func Test_getArticle(t *testing.T) {
+	articleStore := &mock.ArticleService{}
+	userStore := &mock.UserService{}
+	srv := testServer()
+	srv.articleService = articleStore
+	srv.userService = userStore
+
+	token, err := generateUserToken(
+		&model.User{
+			ID:       1,
+			Username: "username",
+		},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/api/v1/articles/{string}", nil)
+	req.Header.Add("Authorization", strings.Join([]string{"Bearer", token}, " "))
+	w := httptest.NewRecorder()
+
+	currentUser := &model.User{
+		Username: "username",
+		Token:    token,
+	}
+	userStore.GetCurrentUserFn = func() *model.User {
+		return currentUser
+	}
+
+	articles := []*model.Article{
+		{
+			Title: "title1",
+			Body:  "body1",
+			Slug:  "slug1",
+		},
+	}
+	articleStore.ArticlesFn = func() ([]*model.Article, error) {
+		return articles, nil
+	}
+	article := articles[0]
+	srv.router.ServeHTTP(w, req)
+	expectedResp := articleResponse(article)
+
+	gotResp := M{}
+	extractResponseArticleBody(w.Body, &gotResp)
+	if code := w.Code; code != http.StatusOK {
+		t.Errorf("expected status code of 200, but got %d", code)
+	}
+
+	if !reflect.DeepEqual(expectedResp, gotResp) {
+		t.Errorf("expected response %v, but got %v", expectedResp, gotResp)
+	}
+}
+
 func extractResponseArticleBody(body io.Reader, v interface{}) {
 	mm := M{}
 	_ = readJSON(body, &mm)
